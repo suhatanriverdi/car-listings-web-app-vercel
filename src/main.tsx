4db7faa50a3bae2a9499160ec0afe0b2671d6ca3
shtanriverdi <shtanriverdi@yahoo.com>
2024-11-11 16:48:34 +0300

    test 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// React Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Main Layout
+import LayoutPage from "./views/LayoutPage.tsx";
+
+// Pages
+import HomePage from "./views/HomePage.tsx";
+import CarDetailsPage from "./views/CarDetailsPage.tsx";
+import ErrorPage from "./views/ErrorPage.tsx";
+import TradeInFormPage from "./views/TradeInFormPage.tsx";
+import LoginPage from "./views/LoginPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutPage />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "details/:id",
+        element: <CarDetailsPage />,
+      },
+      {
+        path: "trade",
+        element: <TradeInFormPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
